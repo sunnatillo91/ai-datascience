d2db1acb78053fd2da6724973f4873ec0b15d19f
Sunnatillo <sunnatillo19912006@gmail.com>
2023-12-23 20:25:32 +0500

    GEN-105 for loop added

--- 9-dars for loop.py
+++ 9-dars for loop.py
@@ -35,11 +35,30 @@ Created on Fri Dec 22 19:58:53 2023
 
 # AMALIYOT
 # Kamida 5 elementdan iborat ismlar degan ro'yxat tuzing, va ro'yxatdagi har bir ismga takrorlanuvchi xabar yozing
-ismlar = ["Olim", "Salim", "Karim", "Jahon"]
-for ism in ismlar:
-    print("Salom", ism)
-print(f"Kod {ism+1} marta takrorlandi")
-# Yuqoridagi tsikl tugaganidan so'ng, ekranga "Kod n marta takrorlandi" degan xabarni chiqaring (n o'rniga kod necha marta takrorlanganini yozing)
+# ismlar = ["Olim", "Salim", "Karim", "Jahon", "Baxrom"]
+# for ism in ismlar:
+#     print("Salom", ism)
+
+# # Yuqoridagi tsikl tugaganidan so'ng, ekranga "Kod n marta takrorlandi" degan xabarni chiqaring (n o'rniga kod necha marta takrorlanganini yozing)
+# print(f"Kod {len(ismlar)} marta takrorlandi")
 # 10 dan 100 gacha bo'lgan toq sonlar ro'yxatini tuzing. Ro'yxatning xar bir elementining kubini yangi qatordan konsolga chiqaring.
-# Foydalanuvchidan 5 ta eng sevimli kinolarini kiritshni so'rang, va kinolar degan ro'yxatga saqlab oling. Natijani konsolga chiqaring.
-# Foydalanuvchidan bugun nechta odam bilan uchrashganini (suhbatlashganini) so'rang, va har bir suhbatlashgan odamning ismini birma-bir so'rab ro'yxatga yozing. Ro'yxatni konsolga chiqaring.
+# toq_sonlar = list(range(11,100,2))
+# for toq_son in toq_sonlar:
+#     print(toq_son**3)
+# Foydalanuvchidan 5 ta eng sevimli kinolarini kiritshni so'rang, va kinolar degan ro'yxatga saqlab oling. 
+# Natijani konsolga chiqaring.
+# kinolar = []
+# print("5 ta eng yoqtirgan kinolaringiz qaysi? ")
+# for kino in range(5):
+#     kinolar.append(input(f"{kino+1}- kino nomini kiriting >>> "))
+# print(kinolar)
+
+# Foydalanuvchidan bugun nechta odam bilan uchrashganini (suhbatlashganini) so'rang, va har bir suhbatlashgan 
+# odamning ismini birma-bir so'rab ro'yxatga yozing. Ro'yxatni konsolga chiqaring.
+nechta_odam = int(input("Bugun nechta odam bilan suhbatlashdingiz?>>> "))
+suhbatdoshlar = []
+# print(nechta_odam)
+for suhbatdosh in range(nechta_odam):
+    suhbatdoshlar.append(input(f"{suhbatdosh+1}-suhbatdoshingizni kiriting>>> "))
+print(suhbatdoshlar)
+
