83c8f1c59d5ff8c75a1b43473380412c159c9c66
Sunnatillo <sunnatillo19912006@gmail.com>
2023-12-23 20:29:52 +0500

    GEN-105 for loop

--- 9-dars for loop.py
+++ 9-dars for loop.py
@@ -6,52 +6,52 @@ Created on Fri Dec 22 19:58:53 2023
 @author: Sunnatillo
 """
 
-# mehmonlar = ["Ali", "Vali", "Hasan", "Husan", "OLim"]
-# for mehmon in mehmonlar:
-#     print("Salom", mehmon)
+mehmonlar = ["Ali", "Vali", "Hasan", "Husan", "OLim"]
+for mehmon in mehmonlar:
+    print("Salom", mehmon)
     
-# mehmonlar = ["Ali", "Vali", "Hasan", "Husan", "OLim"]
-# for mehmon in mehmonlar:
-#     print(f"Hurmatli {mehmon}, sizni 25-dekabr kuni nahorga oshga taklif qilamiz")
-#     print("Hurmat bilan, Ochilovlar oilasi\n")
+mehmonlar = ["Ali", "Vali", "Hasan", "Husan", "OLim"]
+for mehmon in mehmonlar:
+    print(f"Hurmatli {mehmon}, sizni 25-dekabr kuni nahorga oshga taklif qilamiz")
+    print("Hurmat bilan, Ochilovlar oilasi\n")
 
-# sonlar = list(range(1,11))
-# for son in sonlar:
-#     print(f"{son} ning kvadrati {son**2} ga teng")
+sonlar = list(range(1,11))
+for son in sonlar:
+    print(f"{son} ning kvadrati {son**2} ga teng")
 
-# sonlar = list(range(11))  # 1 dan 10 gacha sonlar ro'yxatini shakllantiramiz
-# sonlar_kvadrati = []  # bo'sh ro'yxat yaratamiz
-# for son in sonlar:    #  sonlardagi har bir son uchun 
-#     sonlar_kvadrati.append(son**2)    # uning kvadratini hisoblab konsolga chiqaramiz
+sonlar = list(range(11))  # 1 dan 10 gacha sonlar ro'yxatini shakllantiramiz
+sonlar_kvadrati = []  # bo'sh ro'yxat yaratamiz
+for son in sonlar:    #  sonlardagi har bir son uchun 
+    sonlar_kvadrati.append(son**2)    # uning kvadratini hisoblab konsolga chiqaramiz
     
-# print(sonlar)
-# print(sonlar_kvadrati) 
+print(sonlar)
+print(sonlar_kvadrati) 
 
-# dostlar = []
-# print("5 ta eng yaqin do'stingizni kim?")
-# for n in range(5):
-#     dostlar.append(input(f"{n+1}-do'stingizni ismini kiriting >>>"))
-# print(dostlar)
+dostlar = []
+print("5 ta eng yaqin do'stingizni kim?")
+for n in range(5):
+    dostlar.append(input(f"{n+1}-do'stingizni ismini kiriting >>>"))
+print(dostlar)
 
 # AMALIYOT
 # Kamida 5 elementdan iborat ismlar degan ro'yxat tuzing, va ro'yxatdagi har bir ismga takrorlanuvchi xabar yozing
-# ismlar = ["Olim", "Salim", "Karim", "Jahon", "Baxrom"]
-# for ism in ismlar:
-#     print("Salom", ism)
+ismlar = ["Olim", "Salim", "Karim", "Jahon", "Baxrom"]
+for ism in ismlar:
+    print("Salom", ism)
 
 # # Yuqoridagi tsikl tugaganidan so'ng, ekranga "Kod n marta takrorlandi" degan xabarni chiqaring (n o'rniga kod necha marta takrorlanganini yozing)
-# print(f"Kod {len(ismlar)} marta takrorlandi")
+print(f"Kod {len(ismlar)} marta takrorlandi")
 # 10 dan 100 gacha bo'lgan toq sonlar ro'yxatini tuzing. Ro'yxatning xar bir elementining kubini yangi qatordan konsolga chiqaring.
-# toq_sonlar = list(range(11,100,2))
-# for toq_son in toq_sonlar:
-#     print(toq_son**3)
+toq_sonlar = list(range(11,100,2))
+for toq_son in toq_sonlar:
+    print(toq_son**3)
 # Foydalanuvchidan 5 ta eng sevimli kinolarini kiritshni so'rang, va kinolar degan ro'yxatga saqlab oling. 
 # Natijani konsolga chiqaring.
-# kinolar = []
-# print("5 ta eng yoqtirgan kinolaringiz qaysi? ")
-# for kino in range(5):
-#     kinolar.append(input(f"{kino+1}- kino nomini kiriting >>> "))
-# print(kinolar)
+kinolar = []
+print("5 ta eng yoqtirgan kinolaringiz qaysi? ")
+for kino in range(5):
+    kinolar.append(input(f"{kino+1}- kino nomini kiriting >>> "))
+print(kinolar)
 
 # Foydalanuvchidan bugun nechta odam bilan uchrashganini (suhbatlashganini) so'rang, va har bir suhbatlashgan 
 # odamning ismini birma-bir so'rab ro'yxatga yozing. Ro'yxatni konsolga chiqaring.
