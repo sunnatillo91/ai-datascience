4502080c922dcba6b081afeb91abc4fdd4eb0822
Sunnatillo <sunnatillo19912006@gmail.com>
2023-12-26 21:47:52 +0500

    for if else elif tasks added

--- 11-dars if elif else.py
+++ 11-dars if elif else.py
@@ -110,20 +110,65 @@ Created on Mon Dec 25 18:30:42 2023
 #     + Agar foydalanuvchi 4 yoshdan kichkina yoki 60 dan katta bo'lsa bepul
 #     + Agar foydalanuvchi 18 dan kichik bo'lsa 10000 so'm
 #     + Agar foydalanuvchi 18 dan katta bo'lsa 20000 so'm
-yosh = int(input("Yoshingiz nechada?>>> "))
-if yosh < 4 or yosh>60:
-    narx = 0
-if yosh >= 4 or yosh < 18:
-    narx = 10000
-if yosh >=18 or yosh <= 60:
-    narx = 20000
-print(f"Sizga muzeyga kirish uchun chipta narxi {narx} so'm")
-# - Foydalanuvchidan ikita son kiritishni so'rang, sonlarni solishtiring va ularning teng yoki katta/kichikligi haqida xabarni chiqaring
+# yosh = int(input("Yoshingiz nechada?>>> "))
+# if yosh < 4 or yosh>60:
+#     narx = 0
+# elif yosh >= 4 and yosh < 18:
+#     narx = 10000
+# elif yosh >=18 and yosh <= 60:
+#     narx = 20000
+# print(f"Sizga muzeyga kirish uchun chipta narxi {narx} so'm")
+# - Foydalanuvchidan ikita son kiritishni so'rang, sonlarni solishtiring va ularning teng yoki katta/kichikligi
+# haqida xabarni chiqaring
+# print("Ikkita son kiriting >>>")
+# a = int(input("1-son>>> "))
+# b = int(input("2-son>>> "))
+# if a>b:
+#     print(f"{a} > {b}")
+# elif a==b:
+#     print(f"{a} = {b}")
+# else:
+#     print(f"{a}<{b}")
+# - `mahsulotlar` degan ro'yxat yarating va kamida 10 ta turli mahsulotni kiriting. Yangi, `savat` 
+# degan bo'sh ro'yxat yarating va foydalanuvchidan savatga kamida 5 ta mahsulot kiritishni so'rang. 
+# Savatdagi elementlarni, `mahsulotlar` ro'yxati bilan solishtiring va qaysi biri ro'yxatda bo'lsa 
+# `"Mahsulot do'konimizda bor"` aks holda, `"Mahsulot do'konimizda yo'q"` degan xabarlarni chiqaring.
+# mahsulotlar = ['osh', 'kabob', 'shashlik', 'tuxumbarak', 'somsa', 'sho\'rva', 'lag\'mon', 'salat', 'lavash', 'tabaka']
+# savat = []    
+# print("Kamida 5 ta maxsulot kiriting:")
 
-# - `mahsulotlar` degan ro'yxat yarating va kamida 10 ta turli mahsulotni kiriting. Yangi, `savat` degan bo'sh ro'yxat yarating va foydalanuvchidan savatga kamida 5 ta mahsulot kiritishni so'rang. Savatdagi elementlarni, `mahsulotlar` ro'yxati bilan solishtiring va qaysi biri ro'yxatda bo'lsa `"Mahsulot do'konimizda bor"` aks holda, `"Mahsulot do'konimizda yo'q"` degan xabarlarni chiqaring.
+# for maxsulot in range(5):
+#     savat.append(input(f"{maxsulot+1} maxsulotni kiriting>> "))
+    
+# for maxsulot in savat:
+#     if maxsulot in mahsulotlar:
+#        print(f"{maxsulot} do'konimizda bor")
+#     else:
+#        print(f"{maxsulot} do'konimizda yo'q")
+       
+# - Yuqoridagi dasturni quyidagicha o'zgartiring: foydalanuvchidan 5 ta mahsulot kiritishni so'rang. 
+# Foydalanuvchi so'ragan va do'konda bor mahsulotlarni yang, bor_mahsulotlar degan ro'yxatga, do'konda yo'q 
+# mahsulotlarni esa mavjud_emas degan ro'yxatga qo'shing.  Agar mavjud_emas ro'yxati bo'sh bo'lsa, "Siz so'ragan 
+# barcha mahsulotlar do'konimizda bor" degan xabarni, aks holda esa "Quyidagi mahsulotlar do'konimizda yo'q: ....." degan xabarni chiqaring.
+mahsulotlar = ['osh', 'kabob', 'shashlik', 'tuxumbarak', 'somsa', 'sho\'rva', 'lag\'mon', 'salat', 'lavash', 'tabaka']
+savat = []
+print("Kamida 5 ta maxsulot kiriting:")
+bor_mahsulotlar = []
+mavjud_emas = []
 
-# - Yuqoridagi dasturni quyidagicha o'zgartiring: foydalanuvchidan 5 ta mahsulot kiritishni so'rang. Foydalanuvchi so'ragan va do'konda bor mahsulotlarni yang, bor_mahsulotlar degan ro'yxatga, do'konda yo'q mahsulotlarni esa mavjud_emas degan ro'yxatga qo'shing.  Agar mavjud_emas ro'yxati bo'sh bo'lsa, "Siz so'ragan barcha mahsulotlar do'konimizda bor" degan xabarni, aks holda esa "Quyidagi mahsulotlar do'konimizda yo'q: ....." degan xabarni chiqaring.
+for maxsulot in range(5):
+    savat.append(input(f"{maxsulot+1} maxsulotni kiriting>> "))
+for maxsulot in savat:
+    if maxsulot in mahsulotlar:
+       bor_mahsulotlar.append({maxsulot})
+    else:
+        mavjud_emas.append({maxsulot})
 
+if len(mavjud_emas) == 0:
+    print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
+if maxsulot not in mahsulotlar:
+    print(f"Quyidagi mahsulotlar do'konimizda yo'q: {mavjud_emas}")
+    
 # - `foydalanuvchilar` degan ro'yxat tuzing, va kamida 5 ta login qo'shing. Foydalanuvchidan yangi login tanlashni so'rang va foydalanuvchi kiritgan loginni `foydalanuvchilar` degan ro'yxatning tarkibi bilan solishtiring. Agar ro'yxatda bunday login mavjud bo'lsa, `"Login band, yangi login tanlang!"` aks holda `"Xush kelibsiz, foydalanuvchi!"` xabarini chiqaring.
 
 # - Foydalanuvchidan biror butun son kiritishni so'rang. Foydalanuvchi kiritgan sonni 2 da 10 gacha bo'lgan sonlardan qay biriga qoldiqsiz bo'linishini konsolga chiqaring.
