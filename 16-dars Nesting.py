5159325697fa99f6d1f4e3654f474bebb6335a13
Sunnatillo <sunnatillo19912006@gmail.com>
2024-01-02 18:26:23 +0500

    nesting tasks added

--- 16-dars Nesting.py
+++ 16-dars Nesting.py
@@ -49,8 +49,8 @@ cars = [car0, car1, car2]  # lug'atlar ro'yxat ichiga joylashtirildi va kodlar h
     
 # cars[0]['model']  #bu orqali cars ro'yxatidagi 0- lugatning model degan kalit so'ziga murojaat qilib uning qiymati olindi
 
-print(f"{cars[1]['rang'].title()} "
-      f"{cars[1]['model'].title()}")
+# print(f"{cars[1]['rang'].title()} "
+#       f"{cars[1]['model'].title()}")
 
 malibus = []
 for n in range(10):
@@ -84,8 +84,8 @@ for malibu in malibus:
         malibu['narx']=35000
 
 
-for malibu in malibus:
-    print(malibu)
+# for malibu in malibus:
+#     print(malibu)
     
     
 # LUG'AT ICHIDA RO'YXAT
@@ -95,10 +95,10 @@ dasturchilar = {
     'maruf':['php', 'javascript'],
     'sunnatillo':['php', 'python']
     }
-for ism, tillar in dasturchilar.items():
-    print(f"\n{ism.title()} quyidagi dasturlash tillarini biladi: ")
-    for til in tillar:
-        print(f"{til.upper()} ", end='')
+# for ism, tillar in dasturchilar.items():
+#     print(f"\n{ism.title()} quyidagi dasturlash tillarini biladi: ")
+#     for til in tillar:
+#         print(f"{til.upper()} ", end='')
 
 # LUG'AT ICHIDA LUG'AT
 hamkasblar = {
@@ -119,13 +119,13 @@ hamkasblar = {
            }
     }
 
-for ism, info in hamkasblar.items():
-    print(f"\n{ism.title()} {info['familya'].title()}, "
-          f"{info['tyil']}-yil da tug'ilgan "
-          f"Ma'lumoti: {info['malumot']}. \n"
-          "Quyidagi dasturlash tillarini biladi: ")
-    for til in info['tillar']:
-        print(til.upper())
+# for ism, info in hamkasblar.items():
+#     print(f"\n{ism.title()} {info['familya'].title()}, "
+#           f"{info['tyil']}-yil da tug'ilgan "
+#           f"Ma'lumoti: {info['malumot']}. \n"
+#           "Quyidagi dasturlash tillarini biladi: ")
+#     for til in info['tillar']:
+#         print(til.upper())
           
 # AMALIYOT
 
@@ -134,34 +134,81 @@ for ism, info in hamkasblar.items():
 buxoriy = {
     'ism':'Abu Abdulloh Muhammad ibn Ismoil ibn Ibrohim al Buxoriy',
     'tyil':810,
-    'kitoblari':'Sahihul Buxoriy'
+    'kitoblari':["Al-jomeʼ as-sahih",  "Al-adab al-mufrad", "At-tarix al-kabir", "At-tarix as-sagʻir"]
     }
 temur = {
     'ism':'Amir Temur',
     'tyil':1336,
-    'kitoblari':"temur tuzuklari"
+    'kitoblari':["temur tuzuklari", "temur tuzuklari", "temur tuzuklari"]
     }
 bobur = {
     'ism':'Zahiriddin muhammad bobur',
     'tyil':1483,
-    'kitoblari':'mubayyin'
+    'kitoblari':["Xatti Boburiy","Boburnoma", 'mubayyin']
     }
 shayx = {
     'ism':'shayx muhammad sodiq muhammad yusuf',
     'tyil':1952,
-    'kitoblari':'tafsiri hilol, kifoya'
+    'kitoblari':["tafsiri hilol", "kifoya", "yolg'on"]
     }
 mashxurlar = [buxoriy, temur, bobur, shayx]
-for mashxur in mashxurlar:
-    print(f"\nTo'liq ismi {mashxur['ism'].title()}, "
-          f"{mashxur['tyil']}-yilda tug'ilgan. "
-          f"Yozgan kitoblari: {mashxur['kitoblari'].title()}")
+# for mashxur in mashxurlar:
+#     print(f"\nTo'liq ismi {mashxur['ism']}, "
+#           f"{mashxur['tyil']}-yilda tug'ilgan. "
+#           f"Yozgan kitoblari: {mashxur['kitoblari']}")
 # - Yuqoridagi lug'atlarga har bir shaxsning mashxur asarlari ro'yxatini ham qo'shing. For tsikli yordamida muallifning ismi 
 # va uning asarlarini konsolga chiqaring.
+# for mashxur in mashxurlar:
+#     print(f"\n{mashxur['ism']} asarlari: {mashxur['kitoblari']}")
 
-# - Oila a'zolaringiz (do'stlaringiz) dan 3 ta sevimli kino-seriali haqida so'rang. Do'stingiz ismi kalit, uning sevimli kinolarini esa ro'yxat ko'rinishida lug'artga saqlang.  Natijani konsolga chiqaring.
+# - Oila a'zolaringiz (do'stlaringiz) dan 3 ta sevimli kino-seriali haqida so'rang. Do'stingiz ismi 
+# kalit, uning sevimli kinolarini esa ro'yxat ko'rinishida lug'atga saqlang.  Natijani konsolga chiqaring.
+kinolar = {
+    'ali':['Terminator', 'bryus li', 'kung fu'],
+    'vali':['Yunus emro', 'abdulhamid', 'qashqirlar makoni'],
+    'olim':['alpomish', 'alisher navoiy', 'tangalik bolalar'],
+    'baxrom':['temur', 'jaloliddin', 'katta o\'yin']
+    }
 
-# - Davlatlar degan lug'at yarating, lug'at ichida bir nechta davlatlar haqida ma'lumotlarni lug'at ko'rinishida saqlang. Har bir davlat haqida ma'lumotni konsolga chiqaring.
+for ism, kinolar in kinolar.items():
+    print(f"\n{ism.title()} ning sevimli kinolari: ")
+    for kino in kinolar:
+        print(kino)
 
-# - Yuqoridagi dasturga o'zgartirish kiriting: konsolga barcha davlatlarni emas, foydalanuvchi so'ragan davlat haqida ma'lumot bering. Agar davlat sizning lug'atingizda mavjud bo'lmasa, "Bizda bu davlat haqida ma'lumot yo'q" degan xabarni chiqaring.
+# - Davlatlar degan lug'at yarating, lug'at ichida bir nechta davlatlar haqida ma'lumotlarni lug'at 
+# ko'rinishida saqlang. Har bir davlat haqida ma'lumotni konsolga chiqaring.
+davlatlar = {
+    "o'zbekiston":{'poytaxti':'Toshkent',
+                   "Maydoni": "— 448,978 km2.",
+                   "aholisi soni":"36 milliondan ortiq",
+                   "Pul birligi":"— soʻm.",},
     
+    "qozogʻiston":{'poytaxti':'Ostona',
+                   "Maydoni": "— 2 million 724,9 ming km² ",
+                   "aholisi soni":"19,17 mln. kishi ",
+                   "Pul birligi":"— Tenge"},
+    
+    "turkmaniston":{'poytaxti':'Ashxobod',
+                   "Maydoni": "— 488,100 km2",
+                   "aholisi soni":"6 million",
+                   "Pul birligi":"— Manat "},
+                   }
+# for davlat, info in davlatlar.items():
+#     print(f"\n{davlat.title()} ning poytaxti: {info['poytaxti']}. "
+#           f"Maydoni: {info['Maydoni']}. "
+#           f"Aholisi: {info['aholisi soni']}, "
+#           f"Pul birligi: {info['Pul birligi']}"
+#           )
+# - Yuqoridagi dasturga o'zgartirish kiriting: konsolga barcha davlatlarni emas, foydalanuvchi 
+# so'ragan davlat haqida ma'lumot bering. Agar davlat sizning lug'atingizda mavjud bo'lmasa, 
+# "Bizda bu davlat haqida ma'lumot yo'q" degan xabarni chiqaring.
+davlat = input("Davlat nomini kiriting:>> ").lower()
+if davlat.lower() in davlatlar:
+   info = davlatlar[davlat]
+   print(f"\n{davlat.capitalize()}ning poytaxti: {info['poytaxti']}. "
+          f"\nMaydoni: {info['Maydoni']}. "
+          f"\nAholisi: {info['aholisi soni']}, "
+          f"\nPul birligi: {info['Pul birligi']}"
+          )
+else:
+    print("Bizda bu davlat haqida ma'lumot yo'q")
