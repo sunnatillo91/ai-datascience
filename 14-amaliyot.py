d9ed81cc3247b39b17546b26fb2fabd32656dfc0
Sunnatillo <sunnatillo19912006@gmail.com>
2024-01-03 21:23:26 +0500

    new updates

--- 14-amaliyot.py
+++ 14-amaliyot.py
@@ -16,7 +16,7 @@ otam = {
         'viloyat':'samarqand',
         'manzili':'Barkamol'
         }
-print(f"Otamning ismi {otam['ismi'].title()}, {otam['tyil']}-yilda {otam['viloyat'].title()} viloyatida tug'ilgan")
+# print(f"Otamning ismi {otam['ismi'].title()}, {otam['tyil']}-yilda {otam['viloyat'].title()} viloyatida tug'ilgan")
 # - Oila a'zolaringizning sevimli taomlari lug'atini tuzing. Lug'atda kamida 5 ta ism-taom jufltigi bo'lsin. Kamida uch kishining 
 # sevimli taomini konsolga chiqaring: `Alining sevimli taomi osh`
 taomlar = {
@@ -26,13 +26,43 @@ taomlar = {
     'olim':'shashlik',
     'baxrom':'gril'
     }
-for ism, taom in taomlar.items():
+# print(f"Alining sevimli taomi {taomlar['ali']}")
+# print(f"Valining sevimli taomi {taomlar['vali']}")
+# print(f"Salimning sevimli taomi {taomlar['salim']}")
+# for ism, taom in taomlar.items():
     # print(taom) 
-    print(f"{ism}ning sevimli taomi {taom}")
+    # print(f"{ism.title()}ning sevimli taomi {taom}")
 # - Python izohli lu'gati tuzing: Lug'atga shu kunga qadar o'rgangan 10 ta so'z (atamani) kiriting (masalan integer, float, string, if,
 # else va hokazo) va har birining qisqacha tarjimasini yozing.
-
+dict_py = {
+    'print':'Ma\'lumotni konsolga chiqarish',
+    'variables':'o\'zgaruvchilar',
+    'string':'matn',
+    'integer':'butun son',
+    'float':'o\'nlik son',
+    'list':'ro\'yxat',
+    'console':'spyderda natijani ko\'rsatadigan oyna',
+    'spyder':'dasturlash muhiti',
+    'dictionary':'lug\'at'
+    }
 # - Foydalanuvchidan biror so'z kiritishni so'rang va so'zning tarjimasini yuqoridagi lug'atdan chiqarib bering. Agar so'z lu'gatda
 # mavjud bo'lmasa, "Bunda so'z mavjud emas" degan xabarni chiqaring.
+word = 'print'
+# word = dict_py.get(input("Biror so'z kiriting:>>> "), "Bunday so'z mavjud emas")
+# print(word)
+
+# for w, info in dict_py.items():
+#     if word in dict_py:
+#         print(f"{word['info']}")
 
 # - Yuqoridagi vazifani `if-else` yordamida qiling va natijani ham foydalanuvchiga tushunarli ko'rinishda chiqaring.
+word = input("Biror so'z kiriting:>>> ")
+if word in dict_py:
+    print(dict_py[word])
+else:
+    print("Bunday so'z mavjud emas")
+
+
+
+
+
