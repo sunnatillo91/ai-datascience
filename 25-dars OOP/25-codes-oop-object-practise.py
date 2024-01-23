e9c50c308b8ddf6c2a26e6d8c5eb055a0bd8646c
Sunnatillo <sunnatillo19912006@gmail.com>
2024-01-23 22:05:07 +0500

    objects added

---
+++ 25-dars OOP/25-codes-oop-object-practise.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+25-DARS OOP obyektlar bilan ishlash AMALIYOT
+
+Created on Tue Jan 23 21:50:18 2024
+
+@author: Sunnatillo
+"""
+
+# AMALIYOT
+# 1. Avto degan yangi klass yarating. Unga avtomobillarga doir bo'lgan bir nechta 
+# xususiyatlar (model, rang, korobka, narh va hokazo) qo'shing. Ayrim xususiyatlarga 
+# standart qiymat bering (masalan, kilometer=0)
+class Avto:
+    def __init__(self, model, rang, korobka, narh, yil, kilometr = 0):
+        self.model = model
+        self.rang = rang
+        self.korobka = korobka
+        self.narh = narh
+        self.yil = yil
+        self.kilometr = kilometr
+
+# 2. Avto ga oid obyektning xususiyatlarini qaytaradigan metodlar yozing
+# get_info() metodi avto haqida to'liq ma'lumotni matn ko'rinishida qaytarsin
+    def get_info(self):
+        return f"{self.korobka} korobka {self.rang} {self.model} narxi - {self.narh} $. Ishlab chiqarilgan yili \
+{self.yil}, {self.kilometr} km yurgan"
+            
+avto1 = Avto("Malibu", "Oq", "avtomat", 35000, 2023)
+print(avto1.get_info())
+# 3. Avto ga oid obyektning xususiyatlarini yangilaydigan metodlar yozing.
+# update_km() metodi son qabul qilib olib, avtomobilning yurgan kilometrajini 
+# yangilab borsin
+
+# 4. Yangi, Avtosalon degan klass yarating va kerakli xususiyatlar bilan to'ldiring 
+# (salon nomi, manzili, sotuvdagi avtomobillar va hokazo)
+
+# 5. Avtosalonga yangi avtomobillar qo'shish uchun metod yozing
+# Avtosalondagi avtomobillar haqida ma'lumot qaytaruvchi metod yozing
+
+# 6. Yuqoridagi obyektlar va ularga tegishli metodlarni tekshirib ko'ring
+
+# 7. dir() funksyasi va __dict__ metodi yordamida o'zingiz yozgan va Pythondagi turli 
+# klass va obyektlarning xususiyatlari va metodlarini toping (dir(str), dir(int) va hokazo)
---
+++ 25-dars OOP/25-codes-oop-objects.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+25-codes OOP Obyektlar bilan ishlash
+
+Created on Tue Jan 23 20:23:42 2024
+
+@author: Sunnatillo
+"""
+class Talaba:
+    def __init__(self, ism, familiya, tyil):
+        self.ism = ism
+        self.familiya = familiya
+        self.tyil = tyil
+        self.bosqich = 1
+        
+    def get_name(self):
+        return self.ism
+    def get_fullname(self):
+        return f"{self.familiya} {self.ism}"
+    def get_info(self):
+        return f"{self.ism} {self.familiya}. {self.bosqich} - talabasi"
+        
+talaba = Talaba("Alijon", "Valiyev", 1996)
+talaba2 = Talaba("Olimjon", "Umarov", 1954)
+# print(talaba.bosqich)
+
+class Fan():
+    """Fan nomli klass"""
+    def __init__(self, nomi):
+        self.nomi = nomi
+        self.talabalar_soni = 0
+        self.talabalar = []
+        
+    def get_info(self):
+        """Fan nomini qaytaradi"""
+        return self.nomi
+        
+    def add_student(self, talaba):
+        """Fanga talabalar qo'shish"""
+        self.talabalar.append(talaba)
+        self.talabalar_soni += 1
+        
+    def get_students(self):
+        """Fanga yozilgan talabalar haqida ma'lumot"""
+        # talabalar = []
+        # for talaba in self.talabalar:
+        #     talabalar.append(talaba.get_fullname())
+        # return talabalar
+        # Yuqoridagi 4 qator kod o'rniga shu vazifani bajaradigan 1 qator kod yozamiz
+        return [talaba.get_fullname() for talaba in self.talabalar]
+    
+def see_methods(klass):
+    return [method for method in dir(klass) if method.startswith("__") is False]
+    
+    
+matem = Fan("Oliy matematika")
+talaba = Talaba("Alijon", "Valiyev", 1996)
+talaba2 = Talaba("Olimjon", "Umarov", 1954)
+matem.add_student(talaba)
+matem.add_student(talaba2)
+# print(matem.get_students())
+
+
+# __dict__ METODI
+print(talaba2.__dict__)
+print(talaba2.__dict__.keys())
