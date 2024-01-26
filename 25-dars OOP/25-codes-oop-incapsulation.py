348dec217b91f7723e7022b666ec086d72e399fe
Sunnatillo <sunnatillo19912006@gmail.com>
2024-01-26 22:00:50 +0500

    incapsulation practise

--- 25-dars OOP/25-codes-oop-incapsulation.py
+++ 25-dars OOP/25-codes-oop-incapsulation.py
@@ -18,7 +18,64 @@ print(avto1.get_num_avto())
 # AMALIYOT
 # Avvalgi darslarimizda yaratgan Shaxs va Talaba klasslariga yopiq xususiyatlar 
 # qo'shing va ularning qiymatini ko'rsatuvchi va o'zgartiruvchi metodlar yozing.
+class Shaxs:
+    odamlar_soni = 0
+    def __init__(self, ism, familiya, tyil, pasport):
+        self.ism = ism
+        self.familiya = familiya
+        self.tyil = tyil
+        self.__pasport = pasport
+        Shaxs.odamlar_soni += 1
+        
+    @classmethod
+    def get_odamlar_soni(cls):
+        return cls.odamlar_soni
+    
+    def get_info(self):
+        info = f"{self.ism} {self.familiya}"
+        return info
+    
+    def get_pasport(self):
+        return self.__pasport
+    
+    def change_pasport(self, new_pasport):
+        self.__pasport = new_pasport
+        return self.__pasport
 
+person = Shaxs("Alijon", "Valiyev. ", 1995, 'AD12345')
+# print(person.get_pasport())
+# print(person.change_pasport("AD3566545"))
+
+class Talaba(Shaxs): # Shaxs-super klas, Talaba-voris klas
+    """Talaba klasi"""
+    __talabalar_soni = 0 # clasga oid xususiyatlarni ham incapsulyatsiya orqali yashirsa bo'ladi
+    def __init__(self, ism, familiya, tyil, pasport, idraqam):
+        """Talabaning xususiyatlari"""
+        super().__init__(ism, familiya, tyil, pasport)
+        self.__idraqam = idraqam
+        self.bosqich = 1
+        Talaba.__talabalar_soni += 1
+
+    @classmethod
+    def get_talabalar_soni(cls):
+        return cls.__talabalar_soni
+        
+    def get_id(self):
+        return self.__idraqam     
+    
+    def change_id(self, new_id):
+        self.__idraqam = new_id
+        return self.__idraqam
+    
+    def get_info(self): # bu metodni qaytadan talaba uchun yozdik mana shu holat polymorphism
+        """Talaba haqida ma'lumot"""
+        info = f"{self.ism} {self.familiya} "
+        info += f"{self.bosqich}-bosqich talabasi. ID: {self.idraqam}"
+        return info
+    
+talaba = Talaba("Valijon", "Hakimov", "AB456554", 2001, "AD123221")
+print(talaba.get_id())
+print(talaba.change_id("ID321456"))
 
 # Yuqoridagi klasslarga talabalar_soni va odamlar_soni degan klassga oid xususiyatlar qo'shing.
 
