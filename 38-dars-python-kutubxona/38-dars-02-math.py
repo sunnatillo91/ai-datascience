b7e1693523378249d710c2e808ec2589ecf0cf3f
Sunnatillo <sunnatillo19912006@gmail.com>
2024-02-05 21:36:09 +0500

    updates

--- 38-dars-python-kutubxona/38-dars-02-math.py
+++ 38-dars-python-kutubxona/38-dars-02-math.py
@@ -9,28 +9,38 @@ Created on Mon Feb  5 20:59:54 2024
 
 import math
 
-PI = math.pi
-print(f"PI ning qiymati: {PI}")
-E = math.e
-print(f"e ning qiymati: {E}")
+# PI = math.pi
+# print(f"PI ning qiymati: {PI}")
+# E = math.e
+# print(f"e ning qiymati: {E}")
 
-# Trigonometriya
-print(math.sin(math.pi/2))
-print(math.cos(0))
-print(math.tan(PI))
+# # Trigonometriya
+# print(math.sin(math.pi/2))
+# print(math.cos(0))
+# print(math.tan(PI))
 
-# Radianlar va burchaklar o'rtasida konvertatsiya
-print(math.degrees(math.pi/2))
-print(math.radians(90))
+# # Radianlar va burchaklar o'rtasida konvertatsiya
+# print(math.degrees(math.pi/2))
+# print(math.radians(90))
 
-# Logarifmlar
-# natural logarifm
-print(math.log(5))
-# 10 lik logarifm
-print(math.log10(100))
+# # Logarifmlar
+# # natural logarifm
+# print(math.log(5))
+# # 10 lik logarifm
+# print(math.log10(100))
 
-# Sonlarni yaxlitlash
-x = 4.4
-print(math.ceil(x))  # yuqoriga yaxlitlash
-print(math.floor(x))   # pastga yaxlitlash
-print(round(x))     # yaxlitlash
+# # Sonlarni yaxlitlash
+# x = 4.4
+# print(math.ceil(x))  # yuqoriga yaxlitlash
+# print(math.floor(x))   # pastga yaxlitlash
+# print(round(x))     # yaxlitlash
+
+# Kvadrat ildiz
+x = 81
+print(math.sqrt(x))
+
+# Darajaga oshirish
+x = 3
+print(math.pow(x, 3))  # x ning kubi
+print(math.pow(x, 5))  # x ning 5- darajasi
+print(math.pow(x, 1/3))  # x ning kub ildizi
