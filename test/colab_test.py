fddf8ef353eb1afdb9052c2c07ae35e89623191f
Sunnatillo <sunnatillo19912006@gmail.com>
2024-02-14 20:52:19 +0500

    colab test

---
+++ test/colab_test.py
@@ -0,0 +1,15 @@
+# kutubxonalarni tekshirish
+import numpy
+import pandas as pd
+import torch
+import tensorflow as tf
+import sklearn
+
+# versiyalarni tekshirish
+print(np.__version__)
+print(pd.__version__)
+print(torch.__version__)
+print(tf.__version__)
+print(sklearn.__version__)
+  
+  
